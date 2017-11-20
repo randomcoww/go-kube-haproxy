faed3d4cd7efedecd919252205d10b3f0d2246d8
randomcoww <randomcoww@desktop.lan>
2017-11-19 18:24:47 -0800

    update

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,4 @@ ENV HAPROXY_CONFIG_PATH /etc/haproxy/haproxy.cfg
 ENV HAPROXY_PID_PATH /run/haproxy.pid
 
 COPY go-kube-haproxy /
-COPY haproxy.sh /
 COPY kubeapi.sh /
-
-ENTRYPOINT []
-CMD []
--- haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# echo -en "$CONFIG" > /etc/haproxy/haproxy.cfg
-
-## start
-rm -f $HAPROXY_PID_PATH
-exec haproxy "$@" -V \
-  -f $HAPROXY_CONFIG_PATH \
-	-p $HAPROXY_PID_PATH
